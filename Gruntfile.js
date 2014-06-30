e82e9bd3f5d78ed6106cfeb6c9a111c81f156234
Kevin Smith <kesmith@backcountry.com>
2014-06-30 12:09:25 -0600

    initial commit - grunt and QUnit setup

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        connect: {
+            test: {
+                port: 3000
+            }
+        },
+
+        jshint: {
+            // Define the files to lint
+            files: ['Gruntfile.js', 'solutions/**/*.js', 'test/**/*.js'],
+            // Configure JSHint
+            options: {
+                globals: {
+                    module:     true,
+                    console:    true,
+                    $:          true,
+                    jQuery:     true
+                }
+            }
+        },
+
+        qunit: {
+            files: ['tests/**/*.html']
+        },
+
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint', 'qunit']
+        }
+    });
+
+    // Load all grunt plugins
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Run with "grunt test" on the command line
+    grunt.registerTask('test', ['jshint', 'qunit']);
+
+    // Run with "grunt" on the command line
+    grunt.registerTask('default', ['jshint', 'qunit']);
+};
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-code-challenges
-===============
+# Code Challenges
 
-Various algorithm and code problems
+Various algorithm and code problems, for both fun and interview preparation.
+
+
+## Tests
+```sh
+npm install # Install project dependencies
+grunt test  # Run QUnint tests from the command line
+```
