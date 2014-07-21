042e96e038dc7845c269516e37b5903385e62e87
Kevin Smith <kesmith@backcountry.com>
2014-07-21 15:16:34 -0600

    alter reverse linked list tests to prevent other tests from interfering

--- tests/javascript/reverse-linked-list.js
+++ tests/javascript/reverse-linked-list.js
@@ -10,84 +10,84 @@ function(linkedList) {
         }
     });
 
-    // Reset the linked list before each test group
-    QUnit.testStart(function() {
-        thisList = linkedList.makeLinkedList();
-    });
-
     test('Build a Linked List with the add method', function() {
-        thisList.add('ace of spades');
-        equal(thisList.length, 1, 'Correctly calculates the length of the new list');
-        equal(thisList.tail.value, 'ace of spades', 'Adds a single value to the end of the list');
-        equal(thisList.head.value, 'ace of spades', 'Creates the head of the list');
+        thisList1 = linkedList.makeLinkedList();
+        thisList1.add('ace of spades');
+        equal(thisList1.length, 1, 'Correctly calculates the length of the new list');
+        equal(thisList1.tail.value, 'ace of spades', 'Adds a single value to the end of the list');
+        equal(thisList1.head.value, 'ace of spades', 'Creates the head of the list');
 
-        thisList.add('king of diamonds');
-        equal(thisList.length, 2, 'Adds a second value to the list, and gives correct length');
-        equal(thisList.tail.value, 'king of diamonds', 'Adds a second value to end the list');
-        equal(thisList.head.value, 'ace of spades', 'Head of the list remains unchanged');
+        thisList1.add('king of diamonds');
+        equal(thisList1.length, 2, 'Adds a second value to the list, and gives correct length');
+        equal(thisList1.tail.value, 'king of diamonds', 'Adds a second value to end the list');
+        equal(thisList1.head.value, 'ace of spades', 'Head of the list remains unchanged');
     });
 
     test('Build a Linked List with the prepend method', function() {
-        thisList.prepend('queen of clubs');
-        equal(thisList.length, 1, 'Correctly calculates the length of the new list');
-        equal(thisList.head.value, 'queen of clubs', 'Adds a single value to the start of the list');
-        equal(thisList.tail.value, 'queen of clubs', 'Creates the tail of the list');
+        thisList2 = linkedList.makeLinkedList();
+        thisList2.prepend('queen of clubs');
+        equal(thisList2.length, 1, 'Correctly calculates the length of the new list');
+        equal(thisList2.head.value, 'queen of clubs', 'Adds a single value to the start of the list');
+        equal(thisList2.tail.value, 'queen of clubs', 'Creates the tail of the list');
 
-        thisList.prepend('jack of hearts');
-        equal(thisList.length, 2, 'Correctly calculates the length of the new list');
-        equal(thisList.head.value, 'jack of hearts', 'Adds a single value to the start of the list');
-        equal(thisList.tail.value, 'queen of clubs', 'Tail of the list remains unchanged');
+        thisList2.prepend('jack of hearts');
+        equal(thisList2.length, 2, 'Correctly calculates the length of the new list');
+        equal(thisList2.head.value, 'jack of hearts', 'Adds a single value to the start of the list');
+        equal(thisList2.tail.value, 'queen of clubs', 'Tail of the list remains unchanged');
     });
 
     test('Find if an item is in the list using the contains method', function() {
-        thisList.add('deuce of clubs');
-        thisList.add('three of clubs');
-        thisList.add('four of clubs');
+        thisList3 = linkedList.makeLinkedList();
+        thisList3.add('deuce of clubs');
+        thisList3.add('three of clubs');
+        thisList3.add('four of clubs');
 
-        var test1 = thisList.contains('deuce of clubs');
-        var test2 = thisList.contains('four of hearts');
+        var test1 = thisList3.contains('deuce of clubs');
+        var test2 = thisList3.contains('four of hearts');
         equal(test1, true, 'Correctly returs true for a value that is in the list');
         equal(test2, false, 'Correctly returns false for an item not in the list');
     });
 
     test('Remove an item from the list using the remove method', function() {
-        thisList.add('deuce of spades');
-        thisList.add('three of spades');
-        thisList.add('four of spades');
-        thisList.add('five of spades');
-        thisList.add('six of spades');
+        thisList4 = linkedList.makeLinkedList();
+        thisList4.add('deuce of spades');
+        thisList4.add('three of spades');
+        thisList4.add('four of spades');
+        thisList4.add('five of spades');
+        thisList4.add('six of spades');
 
-        thisList.remove('three of spades');
-        equal(thisList.contains('three of spades'), false, 'Item has been successfully removed from the middle of the list');
+        thisList4.remove('three of spades');
+        equal(thisList4.contains('three of spades'), false, 'Item has been successfully removed from the middle of the list');
 
-        thisList.remove('ace of clubs');
-        equal(thisList.length, 4, 'Item not in the list does not change the list at all');
+        thisList4.remove('ace of clubs');
+        equal(thisList4.length, 4, 'Item not in the list does not change the list at all');
 
-        thisList.remove('deuce of spades');
-        equal(thisList.contains('deuce of spades'), false, 'Item is removed from the head of the list');
-        equal(thisList.head.value, 'four of spades', 'Head of the list is now the second item');
+        thisList4.remove('deuce of spades');
+        equal(thisList4.contains('deuce of spades'), false, 'Item is removed from the head of the list');
+        equal(thisList4.head.value, 'four of spades', 'Head of the list is now the second item');
 
-        thisList.remove('six of spades');
-        equal(thisList.contains('six of spades'), false, 'Item is removed from the tail of the list');
-        equal(thisList.tail.value, 'five of spades', 'Tail of the list is the previous second-to-last item');
+        thisList4.remove('six of spades');
+        equal(thisList4.contains('six of spades'), false, 'Item is removed from the tail of the list');
+        equal(thisList4.tail.value, 'five of spades', 'Tail of the list is the previous second-to-last item');
 
-        thisList.remove('four of spades');
-        thisList.remove('five of spades');
-        equal(thisList.length, 0, 'All items have been removed from the list');
-        equal(thisList.head, null, 'Head is a null pointer for an empty list');
-        equal(thisList.tail, null, 'Tail is a null pointer for an empty list');
+        thisList4.remove('four of spades');
+        thisList4.remove('five of spades');
+        equal(thisList4.length, 0, 'All items have been removed from the list');
+        equal(thisList4.head, null, 'Head is a null pointer for an empty list');
+        equal(thisList4.tail, null, 'Tail is a null pointer for an empty list');
     });
 
     test('Reverse method reverses the order of all nodes in the list', function() {
-        thisList.add('deuce of diamonds');
-        thisList.add('three of diamonds');
-        thisList.add('four of diamonds');
-        thisList.add('five of diamonds');
-        thisList.add('six of diamonds');
+        thisList5 = linkedList.makeLinkedList();
+        thisList5.add('deuce of diamonds');
+        thisList5.add('three of diamonds');
+        thisList5.add('four of diamonds');
+        thisList5.add('five of diamonds');
+        thisList5.add('six of diamonds');
 
-        thisList.reverse();
-        equal(thisList.tail.value, 'deuce of diamonds', 'Tail of the list is the old head');
-        equal(thisList.head.value, 'six of diamonds', 'Head of the list is the old tail');
-        equal(thisList.contains('deuce of diamonds'), true, 'Contains method can still find the tail of the new list');
+        thisList5.reverse();
+        equal(thisList5.tail.value, 'deuce of diamonds', 'Tail of the list is the old head');
+        equal(thisList5.head.value, 'six of diamonds', 'Head of the list is the old tail');
+        equal(thisList5.contains('deuce of diamonds'), true, 'Contains method can still find the tail of the new list');
     });
 });
